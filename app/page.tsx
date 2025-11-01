eb95d065ce62720f843ffd4215e807e192c4d639
DikshantJangra <dikshantjangra1@gmail.com>
2025-11-01 23:42:10 +0530

    Login Signup initiated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      HopeRxPharma
+    <div className="flex justify-between p-5">
+      <h1 className="text-2xl font-medium">
+        <span>Hope</span>
+        <span className="text-emerald-600 font-bold">rx</span>
+        <span>pharma</span>
+      </h1>
+      <div className="flex gap-4">
+        <Link href="/login">Login</Link>
+        <Link href="/signup">Signup</Link>
+      </div>
     </div>
   )
 }
