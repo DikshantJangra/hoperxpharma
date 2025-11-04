9b1e1b60d7e8370b9f788853e053e913c1fa3559
DikshantJangra <dikshantjangra1@gmail.com>
2025-11-05 01:43:50 +0530

    Fronted cros fetch updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const authRoutes = require("./routes/authRouts.js")
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "https://hoperxpharma.onrender.com"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://hoperxpharma.onrender.com", "https://hoperxpharma.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
