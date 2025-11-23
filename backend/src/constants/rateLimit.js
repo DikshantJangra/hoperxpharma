3a0622c7f26675a1e9370add9404e30f71b42e60
DikshantJangra <dikshantjangra1@gmail.com>
2025-11-23 19:51:21 +0530

    API Req updates

--- backend/src/constants/rateLimit.js
+++ backend/src/constants/rateLimit.js
@@ -1,7 +1,7 @@
 const RATE_LIMIT = {
     WINDOW_MS: 15 * 60 * 1000, // 15 minutes
     MAX_REQUESTS: 100,
-    AUTH_MAX_REQUESTS: 5,
+    AUTH_MAX_REQUESTS: 20, // Increased for testing and legitimate retries
 };
 
 module.exports = {
